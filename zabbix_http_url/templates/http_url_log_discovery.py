c379cee039336195c8b36b1ab15cce6856a9a807
haifeng <haifengsss@163.com>
2018-05-11 17:28:59 +0800

    modify http_url_log_discovery.py ,delete chinese

--- zabbix_http_url/templates/http_url_log_discovery.py
+++ zabbix_http_url/templates/http_url_log_discovery.py
@@ -3,32 +3,31 @@
 import json
 import re
 
-with open("{{/usr/local/zabbix/script}}/http_url.txt") as f:
+with open("{{zabbix_script_dir}}/http_url.txt") as f:
     fdata = f.readlines()
 
 r1 = re.compile(r'^#|^$')
 l1 = []
-# 检测空行和注释
+# check null and comment
 for i in fdata:
     if r1.search(i) == None:
         l1.append(i)
 
 fdata = l1
-print len(fdata)
-# 将每行按制表符分割
+# split by '\t' for per line
 fdata = [i.strip('\n').split('\t') for i in fdata]
 
-# 如果不能按制表符分割的行，则按空格分割
+# split by space for per line
 for i in range(len(fdata)):
     if len(fdata[i])==1:
         fdata[i] = fdata[i][0].split()
 
-# 再次检测异常的行，如果有，将其删除
+# delete unusual
 l2 = []
-for i in range(len(fdata)):
+{%raw%}for i in range(len(fdata)):
     if len(fdata[i]) == 2:
         l2.append(fdata[i])
-
+{%endraw%}
 fdata = l2
 
 for i in fdata:
@@ -36,9 +35,7 @@ for i in fdata:
 
 
 fdata = [ {"{%raw%}{#DSCODE}{%endraw%}":i[0], "{%raw%}{#DSADDR}{%endraw%}": i[1]} for i in fdata ]
-print len(fdata)
 
 d1 = {'data': fdata}
 data = json.dumps(d1)
 print data
-
