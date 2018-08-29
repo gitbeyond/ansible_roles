faa73b23c50ebdc69275ef2eda22ed2b0a67eb77
haifeng <haifengsss@163.com>
2018-08-29 11:35:12 +0800

    modified jdk_install at install jdk to 9.65

--- jdk_install/templates/java.sh
+++ jdk_install/templates/java.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=/usr/local/{{jdk_dir_name}}
+export JAVA_HOME={{install_dir}}/{{jdk_dir_name}}
 export JRE_HOME=${JAVA_HOME}/jre
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
