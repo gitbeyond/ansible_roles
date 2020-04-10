01452295f2378e705997e2ecdb97aacdcb86cc9b
haifeng_qd <wanghaifeng@geotmt.com>
2020-04-10 14:43:34 +0800

    updated mysql_user and lvs_realserver at deploy lvs_realserver on 25.34/35.

--- lvs_realserver/templates/check_realserver.sh
+++ lvs_realserver/templates/check_realserver.sh
@@ -8,7 +8,7 @@ metric_name=node_ipvs_realserver_status
 check_realserver(){
     echo "# HELP ${metric_name} realserver running status, 0 : error, 1 : running"
     echo "# TYPE ${metric_name} gauge"
-    /etc/init.d/realserver status
+    /etc/init.d/realserver status &> /dev/null
     if [ $? == 0 ];then
         echo "${metric_name} 1"
     else
@@ -16,7 +16,7 @@ check_realserver(){
     fi
 }
 
-if [ -d ${metric_file_full_path} ];then 
+if [ -d ${node_exporter_textfile_directory} ];then 
     check_realserver > ${metric_file_full_path}_1 && /bin/mv ${metric_file_full_path}_1 ${metric_file_full_path}
 else
     exit 1
--- mysql_user/templates/mysql.sh
+++
@@ -1,2 +0,0 @@
-export MYSQL_HOME={{mysql_base_dir}}
-export PATH=${MYSQL_HOME}/bin:$PATH
