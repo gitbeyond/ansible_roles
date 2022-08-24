8f2c685233e93938a9a92679455c3bdd1d527a03
铁皮石斛 <haifengsss@163.com>
2022-08-24 14:11:37 +0800

    regular update.

--- dockerFiles/jdk/entrypoint.sh
+++ dockerFiles/jdk/entrypoint.sh
@@ -6,16 +6,24 @@ project_name=${project_name%-*}
 project_jar_file=$(ls ${project_base_dir}/${project_name}*.jar)
 
 if [ -n "${project_jar_file}" ];then
-    :
+    if [ -e "${project_jar_file}" ];then
+        :
+    else
+        echo "The jar_file:${project_jar_file} isn't exist."
+        exit 3
+    fi
 else
     echo "There isn't a jar file in the ${project_base_dir}."
     exit 2
 fi
 
-if [ -e "${project_jar_file}" ];then
-    echo "java ${JAVA_OPTS} -jar ${project_jar_file} ${project_run_opts}"
-    exec java ${JAVA_OPTS} -jar ${project_jar_file} ${project_run_opts}
+if [ -n "${JAVA_OPTS}" ];then
+    :
 else
-    echo "The jar_file:${project_jar_file} isn't exist."
-    exit 3
+    if [ -n "${CATALINA_OPTS}" ];then
+        JAVA_OPTS="${CATALINA_OPTS}"
+    fi
 fi
+
+echo "java ${JAVA_OPTS} -jar ${project_jar_file} ${project_run_opts}"
+exec java ${JAVA_OPTS} -jar ${project_jar_file} ${project_run_opts}
